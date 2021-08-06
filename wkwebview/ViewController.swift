e849b4c061236f258f3a10dd603b0e66cde9b84a
amir <amyrsalarr@gmail.com>
2021-08-06 13:37:36 +0430

    first commit

--- wkwebview/ViewController.swift
+++ wkwebview/ViewController.swift
@@ -7,23 +7,25 @@
 
 import UIKit
 import WebKit
+import Foundation
+import AVFoundation
 
-class ViewController: UIViewController, WKNavigationDelegate {
+
+class WebViewController: UIViewController, WKNavigationDelegate {
+  
     var webView: WKWebView!
-
+    
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
-        view = webView
+        view .addSubview(<#T##view: UIView##UIView#>)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://www.google.com")!
+        let url = URL(string: "https://sorfeh.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
-
-
 }
 
